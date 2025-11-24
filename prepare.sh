7ed123d4c0814752e0efd42ac503f646966c2837
Frederick Vollbrecht <frederick@vollbrecht.cc>
2025-11-24 15:36:36 +0100

    update gradle version

--- prepare.sh
+++ prepare.sh
@@ -11,5 +11,4 @@ cp build/distributions/qftest-jpro-javafx-playground-jpro.zip runtime/
 
 # download loadbalancer
 cd runtime
-URL="curl -LO https://sandec.jfrog.io/artifactory/repo/one/jpro/jpro-loadbalancer/0.14.0-SNAPSHOT/jpro-loadbalancer-0.14.0-SNAPSHOT.jar"
-curl -LO https://sandec.jfrog.io/artifactory/repo/one/jpro/jpro-loadbalancer/0.14.0-SNAPSHOT/jpro-loadbalancer-0.14.0-SNAPSHOT.jar
+curl -LO https://sandec.jfrog.io/artifactory/repo/one/jpro/jpro-loadbalancer/2025.3.1/jpro-loadbalancer-2025.3.1.jar
